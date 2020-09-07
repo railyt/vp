ca5711a035b882a0627bb6b0e941175d4c095f65
Raily Toomingas <railyt@users.noreply.github.com>
2020-09-07 17:29:01 +0300

    teise tunni asjad

--- tund2/home.php
+++ tund2/home.php
@@ -12,6 +12,14 @@
 	if($hournow>=8 and $hournow <18){
 		$partofday="õppimise aeg";
 	}
+	
+	//jälgime semestri kulgu
+	$semesterstart=new DateTime("2020-8-31");
+	$semesterend=new DateTime("2020-12-13");
+	$semesterduration=$semesterstart->diff($semesterend);
+	$today=new DateTime("now");
+	$fromsemesterstart=$semesterstart->diff($today);  //saime aja erinevuse objektina, seda niisama näidata ei saa
+	$fromsemesterstartdays=$fromsemesterstart->format("%r%a");
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -24,7 +32,7 @@
 	<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
 	<h1><?php echo $username;?> programmeerib veebi</h1>
 	<p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
-	<p>Lehe avamise aeg: <?php echo $fulltimenow;?>. 
+	<p>Lehe avamise aeg: <?php echo $fulltimenow.", semestri algusest on möödunud ".$fromsemesterstartdays." päeva";?>. 
 	<?php echo "Parajasti on " .$partofday.".";?></p>
 	<p>Leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee/">Tallinna Ülikooli</a> Digitehnoloogiate instituudis. </p>
 </body>
